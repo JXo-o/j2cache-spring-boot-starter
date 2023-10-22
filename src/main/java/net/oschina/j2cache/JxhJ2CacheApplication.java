d4e45f43fdc48e0265dc011dc0df5b81c08ed237
贾星昊 <23120357@bjtu.edu.cn>
2023-10-22 23:32:11 +0800

    创建springboot3项目

---
+++ src/main/java/net/oschina/j2cache/JxhJ2CacheApplication.java
@@ -0,0 +1,13 @@
+package net.oschina.j2cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxhJ2CacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxhJ2CacheApplication.class, args);
+    }
+
+}
